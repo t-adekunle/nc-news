5c4e9c98d89cbbf749460631b05243f1c370b763
t-adekunle <tayoadekunle97@gmail.com>
2024-03-12 13:53:22 +0000

    amended CommentList to include loading state and logic for no comments

--- src/CommentsList.jsx
+++ src/CommentsList.jsx
@@ -1,32 +1,41 @@
-import { useState, useEffect } from "react"
-import { fetchCommentsByArticle } from "./api"
+import { useState, useEffect } from "react";
+import { fetchCommentsByArticle } from "./api";
+import Loading from "./Loading";
 
-const CommentsList = ({article}) =>{
-    const [comments, setComments] = useState([])
- 
-    useEffect(() => {
-        fetchCommentsByArticle(article.article_id).then((data)=>{
-            
-            setComments(data)
-        })
-    }, [])
+const CommentsList = ({ article }) => {
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    return(
-        <div>
-            {comments.map((comment) => {
-                return <div key={comment.comment_id} className = 'comment'>
-                    <p className = 'comment-author'>{comment.author}</p>
-                    <p className = 'comment-created-at'>{comment.created_at}</p>
-                    <p className = 'comment-body'>{comment.body}</p>
-                    <p className = 'comment-votes'>Votes: {comment.votes}</p>
-                    <button>Vote &#8679;</button>
-                    <button>Delete</button>
-                </div>
-            })}
-        </div>
-    )
-}
+  useEffect(() => {
+    setIsLoading(true);
+    fetchCommentsByArticle(article.article_id).then((data) => {
+      setComments(data);
+      setIsLoading(false);
+    });
+  }, []);
 
+  if (isLoading) {
+    return <Loading />;
+  } else if (comments.length === 0) {
+    return <p>This article has no comments</p>;
+  } else {
+    return (
+      <div>
+        {comments.map((comment) => {
+          return (
+            <div key={comment.comment_id} className="comment">
+              <p className="comment-author">{comment.author}</p>
+              <p className="comment-created-at">{comment.created_at}</p>
+              <p className="comment-body">{comment.body}</p>
+              <p className="comment-votes">Votes: {comment.votes}</p>
+              <button>Vote &#8679;</button>
+              <button>Delete</button>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+};
 
-
-export default CommentsList
+export default CommentsList;
