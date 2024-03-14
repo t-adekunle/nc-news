547a735a9c87ee0ec546db02ae994086d35162c2
t-adekunle <tayoadekunle97@gmail.com>
2024-03-14 12:14:18 +0000

    wrapped app in user context

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { UserContext } from './contexts/User'
 
-
+const [loggedInUser, setLoggedInUser ] = useState({})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
+  <UserContext.Provider value={{loggedInUser, setLoggedInUser}}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
- 
+  </UserContext.Provider>
 )
